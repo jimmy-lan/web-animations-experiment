d6cd2863ed5ec0bea55a6038e0b2fe28ea9f7748
Jimmy <jimmylanbcn@gmail.com>
2021-07-29 17:44:15 -0400

    Add fade keyframes

--- react-text-animation/src/components/TextAnimation/TextAnimation.style.js
+++ react-text-animation/src/components/TextAnimation/TextAnimation.style.js
@@ -1,5 +1,9 @@
 import { createUseStyles } from "react-jss";
 
 export const useStyles = createUseStyles({
+  "@keyframes fade": {
+    "0%": { opacity: 0 },
+    "100%": { opacity: 1 },
+  },
   animatedSpan: {},
 });
