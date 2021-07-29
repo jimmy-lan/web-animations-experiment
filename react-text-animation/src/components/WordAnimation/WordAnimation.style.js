650a9deca1470987a2015a4fd4f9897e867586b8
Jimmy <jimmylanbcn@gmail.com>
2021-07-29 19:58:00 -0400

    Add word keyframe animation

---
+++ react-text-animation/src/components/WordAnimation/WordAnimation.style.js
@@ -0,0 +1,18 @@
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  "@keyframe animation": {
+    "0%": {
+      transform: "translateY(-30px)",
+      opacity: 0,
+    },
+    "70%": {
+      transform: "translateY(10px)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "translateY(0)",
+      opacity: 1,
+    },
+  },
+});
