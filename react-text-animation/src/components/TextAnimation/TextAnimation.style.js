a4404b7ccb3d7b879a82771b40870144474a32fd
Jimmy <jimmylanbcn@gmail.com>
2021-07-29 18:11:26 -0400

    Apply transform animations

--- react-text-animation/src/components/TextAnimation/TextAnimation.style.js
+++ react-text-animation/src/components/TextAnimation/TextAnimation.style.js
@@ -2,12 +2,22 @@ import { createUseStyles } from "react-jss";
 
 export const useStyles = createUseStyles({
   "@keyframes fade": {
-    "0%": { opacity: 0 },
-    "100%": { opacity: 1 },
+    "0%": {
+      opacity: 0,
+      transform: "translateY(60px) skewY(8deg)",
+      filter: "blur(6px)",
+    },
+    "100%": {
+      opacity: 1,
+      transform: "translateY(0) skewY(0)",
+      filter: "blur(0)",
+    },
   },
   animatedSpan: {
+    display: "inline-block",
     animationName: "$fade",
-    animationDuration: "6s",
+    animationDuration: "1.5s",
+    animationTimingFunction: "ease-in-out",
     fontSize: "5rem",
     fontWeight: 500,
     letterSpacing: 1.2,
