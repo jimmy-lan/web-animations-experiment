76a6e19de1ca3dce112caad5c36922d9bcbb2abd
Jimmy <jimmylanbcn@gmail.com>
2021-07-31 13:53:17 -0400

    Use WordAnimation

--- react-text-animation/src/App.jsx
+++ react-text-animation/src/App.jsx
@@ -1,12 +1,13 @@
 import CharacterAnimation from "./components/CharacterAnimation/CharacterAnimation";
 import { useStyles } from "./App.style";
+import WordAnimation from "./components/WordAnimation/WordAnimation";
 
 function App() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <CharacterAnimation content="React" />
+      <WordAnimation content="I love React" />
     </div>
   );
 }
