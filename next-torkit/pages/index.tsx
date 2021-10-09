0cffe093325304542e04cbc85427bfe154969deb
Jimmy <jimmylanbcn@gmail.com>
2021-10-09 15:11:45 -0400

    Add sample text

--- next-torkit/pages/index.tsx
+++ next-torkit/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import React from "react";
 
 const Home: NextPage = () => {
-  return <div id="sample"></div>;
+  return <div id="sample">Hi there</div>;
 };
 
 export default Home;
