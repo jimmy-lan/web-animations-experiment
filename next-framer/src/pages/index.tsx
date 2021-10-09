75eedb9a451d2054a332ba1a91ed7147bf7702a5
Jimmy <jimmylanbcn@gmail.com>
2021-10-09 19:56:49 -0400

    Add RevealTypography component

--- next-framer/src/pages/index.tsx
+++ next-framer/src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import React from "react";
 import { Paper, Typography } from "@material-ui/core";
 import { useStyles } from "./Home.style";
+import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
   const classes = useStyles();
