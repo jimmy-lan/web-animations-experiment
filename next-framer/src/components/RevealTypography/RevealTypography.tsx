dec40594ac6aa50267c8d0cc5493df1a068a39eb
Jimmy <jimmylanbcn@gmail.com>
2021-10-09 19:57:42 -0400

    Add RevealTypography.tsx

---
+++ next-framer/src/components/RevealTypography/RevealTypography.tsx
@@ -0,0 +1,12 @@
+import React, {FunctionComponent} from 'react';
+
+interface OwnProps {
+}
+
+type Props = OwnProps;
+
+const RevealTypography: FunctionComponent<Props> = (props) => {
+  return ();
+};
+
+export {RevealTypography};
