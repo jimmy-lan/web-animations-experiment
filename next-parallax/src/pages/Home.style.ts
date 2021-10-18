5ce41588d7dfbf5e29a4a9ddca73e5e32f0dd7a6
Jimmy <jimmylanbcn@gmail.com>
2021-10-17 22:23:49 -0400

    Use parallax component

--- next-parallax/src/pages/Home.style.ts
+++ next-parallax/src/pages/Home.style.ts
@@ -4,6 +4,7 @@ export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       padding: "20%",
+      height: "150vh",
     },
     sectionTitle: {
       marginTop: theme.spacing(6),
--- next-parallax/src/pages/index.tsx
+++ next-parallax/src/pages/index.tsx
@@ -2,13 +2,52 @@ import type { NextPage } from "next";
 import React from "react";
 import { Paper, Typography } from "@material-ui/core";
 import { useStyles } from "./Home.style";
+import { Parallax } from "../components";
 
 const Home: NextPage = () => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.root}>
-      <Typography>Hi there!</Typography>
+    <Paper elevation={0} className={classes.root}>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        aliquid asperiores atque aut, debitis dignissimos ducimus eaque, error
+        exercitationem hic illum laudantium modi molestias porro quas saepe
+        tenetur, vel vitae.
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        aliquid asperiores atque aut, debitis dignissimos ducimus eaque, error
+        exercitationem hic illum laudantium modi molestias porro quas saepe
+        tenetur, vel vitae.
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        aliquid asperiores atque aut, debitis dignissimos ducimus eaque, error
+        exercitationem hic illum laudantium modi molestias porro quas saepe
+        tenetur, vel vitae.
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        aliquid asperiores atque aut, debitis dignissimos ducimus eaque, error
+        exercitationem hic illum laudantium modi molestias porro quas saepe
+        tenetur, vel vitae.
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        aliquid asperiores atque aut, debitis dignissimos ducimus eaque, error
+        exercitationem hic illum laudantium modi molestias porro quas saepe
+        tenetur, vel vitae.
+      </Typography>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        aliquid asperiores atque aut, debitis dignissimos ducimus eaque, error
+        exercitationem hic illum laudantium modi molestias porro quas saepe
+        tenetur, vel vitae.
+      </Typography>
+      <Parallax>
+        <Typography>Hi there!</Typography>
+      </Parallax>
     </Paper>
   );
 };
