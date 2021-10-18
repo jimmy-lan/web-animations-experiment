157e40cfdd79985c86494943bc7986751fd504b4
Jimmy <jimmylanbcn@gmail.com>
2021-10-17 21:55:17 -0400

    Add position states and calculations

--- next-parallax/src/components/Parallax/Parallax.tsx
+++ next-parallax/src/components/Parallax/Parallax.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactNode } from "react";
+import React, { FunctionComponent, ReactNode, useRef, useState } from "react";
 import { motion, useViewportScroll, useTransform } from "framer-motion";
 
 interface Props {
@@ -9,8 +9,16 @@ interface Props {
 type ParallaxProps = Props;
 
 const Parallax: FunctionComponent<ParallaxProps> = (props) => {
-  const { children } = props;
+  const { children, offset = 50 } = props;
+  const [elementTop, setElementTop] = useState(0);
+  const [clientHeight, setClientHeight] = useState(0);
+  const ref = useRef();
+
   const { scrollY } = useViewportScroll();
+
+  const initial = elementTop - clientHeight;
+  const final = elementTop + offset;
+
   const y = useTransform(scrollY, [100, 200], [0, 500]);
 
   return <motion.div style={{ y }}>{children}</motion.div>;
